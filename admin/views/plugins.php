e2326badedac208b0f207d3235ba47b228090100
dignajar <dignajar@gmail.com>
2015-06-28 15:49:24 -0300

    Bug fixes

--- admin/views/plugins.php
+++ admin/views/plugins.php
@@ -9,7 +9,7 @@
 		echo '<p>'.$Plugin->description().'</p>';
 
 		if($Plugin->installed()) {
-			echo '<a href="'.HTML_PATH_ADMIN_ROOT.'uninstall-plugin/'.$Plugin->className().'" class="btn btn-blue btn-small">Uninstall plugin</a>';
+			echo '<a href="'.HTML_PATH_ADMIN_ROOT.'uninstall-plugin/'.$Plugin->className().'" class="btn btn-red btn-small">Uninstall plugin</a>';
 		}
 		else {
 			echo '<a href="'.HTML_PATH_ADMIN_ROOT.'install-plugin/'.$Plugin->className().'" class="btn btn-blue btn-small">Install plugin</a>';
