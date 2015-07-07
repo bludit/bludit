5788392c8ccb85363bc751d833d263b3f2c1a0a9
dignajar <dignajar@gmail.com>
2015-07-06 22:16:30 -0300

    Bug fixes

--- themes/pure-clean/php/page.php
+++ themes/pure-clean/php/page.php
@@ -10,22 +10,6 @@
             <a href="<?php echo $Page->permalink() ?>"><?php echo $Page->title() ?></a>
         </h2>
 
-        <!-- page date and author -->
-        <div class="page-meta">
-            <span class="author">
-                <?php
-                    echo $Language->get('Posted By').' ';
-
-                    if( Text::isNotEmpty($Page->authorFirstName()) && Text::isNotEmpty($Page->authorLastName()) ) {
-                        echo $Page->authorFirstName().', '.$Page->authorLastName();
-                    }
-                    else {
-                        echo $Page->username();
-                    }
-                ?>
-            </span>
-        </div>
-
     </header>
 
     <!-- page content -->
