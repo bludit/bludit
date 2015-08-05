0efce34cf98a6d7cb0e008a81e45be4fc3ec8678
dignajar <dignajar@gmail.com>
2015-08-04 21:42:28 -0300

    minor changes

--- admin/views/themes.php
+++ admin/views/themes.php
@@ -12,7 +12,7 @@
 
 		echo '<p class="name">'.$theme['name'].'</p>';
 		echo '<p>'.$theme['description'].'</p>';
-		echo '<span class="version">'.$Language->g('Version').': '.$theme['version'].'</span><span class="author">'.$Language->g('author').': '.$theme['author'].'</span>';
+		echo '<span class="version">'.$Language->g('Version').': '.$theme['version'].'</span><span class="author">'.$Language->g('author').': <a targe="_blank" href="'.$theme['website'].'">'.$theme['author'].'</a></span>';
 
 		if($theme['dirname']!=$Site->theme()) {
 			echo '<a href="'.HTML_PATH_ADMIN_ROOT.'install-theme/'.$theme['dirname'].'" class="btn btn-red btn-smaller">'.$Language->g('Install theme').'</a>';
