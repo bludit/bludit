bea4c3f169957db9eddda7aaf0b9edbce1c65945
dignajar <dignajar@gmail.com>
2016-01-23 20:51:04 -0300

    Bug fixed, #206

--- bl-plugins/disqus/plugin.php
+++ bl-plugins/disqus/plugin.php
@@ -33,7 +33,7 @@ class pluginDisqus extends Plugin {
 		{
 			global $Site;
 
-			if( !empty($Site->homePage()) ) {
+			if( Text::notEmpty($Site->homePage()) ) {
 				$this->disable = true;
 			}
 		}
