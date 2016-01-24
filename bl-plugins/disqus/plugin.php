642410284f9e072c72fc4af3d56ab5256743f9b3
acrox999 <acrox999@gmail.com>
2016-01-24 23:30:10 +0800

    Fix typo(?) that breaks the functionality.

--- bl-plugins/disqus/plugin.php
+++ bl-plugins/disqus/plugin.php
@@ -33,7 +33,7 @@ class pluginDisqus extends Plugin {
 		{
 			global $Site;
 
-			if( Text::notEmpty($Site->homePage()) ) {
+			if( Text::isNotEmpty($Site->homePage()) ) {
 				$this->disable = true;
 			}
 		}
