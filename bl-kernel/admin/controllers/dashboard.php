0a71ae164aa294d3f10cb59d5e76443b5d19d8fe
dignajar <dignajar@gmail.com>
2016-01-28 20:03:15 -0300

    Pages date updates

--- bl-kernel/admin/controllers/dashboard.php
+++ bl-kernel/admin/controllers/dashboard.php
@@ -11,7 +11,7 @@ function updateBludit()
 	// Check if Bludit need to be update.
 	if( ($Site->currentBuild() < BLUDIT_BUILD) || isset($_GET['update']) )
 	{
-		// --- Update dates ---
+		// --- Update dates on posts ---
 		foreach($dbPosts->db as $key=>$post)
 		{
 			$date = Date::format($post['date'], 'Y-m-d H:i', DB_DATE_FORMAT);
@@ -21,6 +21,17 @@ function updateBludit()
 		}
 
 		$dbPosts->save();
+
+		// --- Update dates on pages ---
+		foreach($dbPages->db as $key=>$page)
+		{
+			$date = Date::format($page['date'], 'Y-m-d H:i', DB_DATE_FORMAT);
+			if($date !== false) {
+				$dbPages->setPostDb($key,'date',$date);
+			}
+		}
+
+		$dbPages->save();
 
 		// --- Update directories ---
 		$directories = array(
