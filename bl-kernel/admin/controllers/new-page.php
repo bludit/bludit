75ea887348a001a757ccc25e07348836f0570553
Kim Keown <kimkeown@users.noreply.github.com>
2016-10-25 06:23:30 -0600

    Revise new-page.php - Admin folder rename
    Add variable in bl-kernel/boot/init.php that allows User to rename bl-kernel/admin folder. 
    User can then define variable in bl-kernel/boot/init.php and change the foldername itself to effect the rename. 
    
    Add global $adminfolder variable as necessary and replace relevant 'admin' strings with $adminfolder.
    Applies to most of the files in bl-kernel/admin/controllers.
    
    Line 15 - Reference Admin folder variable within function
    	       global $adminfolder;
    
    Line 27 - Replace Admin string with folder variable:
    		Redirect::page($adminfolder, 'manage-pages');
    Original: 	Redirect::page('admin', 'manage-pages');

--- bl-kernel/admin/controllers/new-page.php
+++ bl-kernel/admin/controllers/new-page.php
@@ -12,7 +12,8 @@ function addPage($args)
 {
 	global $dbPages;
 	global $Language;
-
+	global $adminfolder;
+	
 	// Add the page, if the $key is FALSE the creation of the post failure.
 	$key = $dbPages->add($args);
 
@@ -23,7 +24,7 @@ function addPage($args)
 
 		// Alert the user
 		Alert::set($Language->g('Page added successfully'));
-		Redirect::page('admin', 'manage-pages');
+		Redirect::page($adminfolder, 'manage-pages');
 	}
 	else
 	{
