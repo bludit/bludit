ec9498c03ad3505382f7d6c9958aa8ecb9d2f016
Kim Keown <kimkeown@users.noreply.github.com>
2016-10-25 06:48:40 -0600

    Revise settings-regional.php- Admin folder rename
    Add variable in bl-kernel/boot/init.php that allows User to rename bl-kernel/admin folder. 
    User can then define variable in bl-kernel/boot/init.php and change the foldername itself to effect the rename. 
    
    Add global $adminfolder as necessary and replace relevant 'admin' strings with $adminfolder.
    Applies to most of the files in bl-kernel/admin/controllers.
    
    Line 3 - Reference Admin folder variable 
                 global $adminfolder;
    
    Line 11 - Replace Admin string with folder variable:
    	        Redirect::page($adminfolder, 'dashboard');
    Original:   	Redirect::page('admin', 'dashboard');
    
    Line 58 - Replace Admin string with folder variable:
    	       Redirect::page($adminfolder, $layout['controller']);
    Original:   	Redirect::page('admin', $layout['controller']);

--- bl-kernel/admin/controllers/settings-regional.php
+++ bl-kernel/admin/controllers/settings-regional.php
@@ -1,12 +1,14 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
+global $adminfolder;
+
 // ============================================================================
 // Check role
 // ============================================================================
 
 if($Login->role()!=='admin') {
 	Alert::set($Language->g('you-do-not-have-sufficient-permissions'));
-	Redirect::page('admin', 'dashboard');
+	Redirect::page($adminfolder, 'dashboard');
 }
 
 // ============================================================================
@@ -53,7 +55,7 @@ function setSettings($args)
 if( $_SERVER['REQUEST_METHOD'] == 'POST' )
 {
 	setSettings($_POST);
-	Redirect::page('admin', $layout['controller']);
+	Redirect::page($adminfolder, $layout['controller']);
 }
 
 // ============================================================================
