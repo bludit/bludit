a720b7ea5c79613732a936602551666aca9dbb69
Hakim Zulkufli <acrox999@gmail.com>
2017-04-30 20:10:34 +0800

    Revert "Quill plugin fix."
    This reverts commit 84aa89a861d6f50572e895e5b851350c13c65e90.

--- bl-plugins/quill/plugin.php
+++ bl-plugins/quill/plugin.php
@@ -56,7 +56,7 @@ class pluginQuill extends Plugin {
 		$html .= '<input name="autosave" id="jsautosave" type="checkbox" value="1" '.($this->getDbField('autosave')?'checked':'').'>';
 		$html .= '<label class="forCheckbox" for="jsautosave">'.$Language->get('Autosave').'</label>';
 		$html .= '</div>';
-
+		
 		$html .= '<div>';
 		$html .= '<input type="hidden" name="spellChecker" value="0">';
 		$html .= '<input name="spellChecker" id="jsspellChecker" type="checkbox" value="1" '.($this->getDbField('spellChecker')?'checked':'').'>';
@@ -72,39 +72,21 @@ class pluginQuill extends Plugin {
 		global $Language;
 
 		$html = '';
-		// Load CSS and JS only on Controllers in array.
-		if(in_array($layout['controller'], $this->loadWhenController))
-		{
+		if( $this->enabled() ) {
 			$html .= '
 			<script>
 
-				var toolbarOptions = [
-						["bold", "italic", "underline", "strike"],        // toggled buttons
-						["blockquote", "code-block"],
-
-						[{ "header": 1 }, { "header": 2 }],               // custom button values
-						[{ "list": "ordered"}, { "list": "bullet" }],
-						[{ "script": "sub"}, { "script": "super" }],      // superscript/subscript
-						[{ "indent": "-1"}, { "indent": "+1" }],          // outdent/indent
-						[{ "direction": "rtl" }],                         // text direction
-
-						[{ "size": ["small", false, "large", "huge"] }],  // custom dropdown
-						[{ "header": [1, 2, 3, 4, 5, 6, false] }],
-
-						[{ "color": [] }, { "background": [] }],          // dropdown with defaults from theme
-						[{ "font": [] }],
-						[{ "align": [] }],
-
-						["clean"]                                         // remove formatting button
-				];
-
-				var quill = new Quill("#jscontent", {
-				  modules: {
-				    toolbar: toolbarOptions
-				  },
-				  placeholder: "Compose an epic...",
-				  theme: "snow"
-				});
+var quill = new Quill("#editor-container", {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline"],
+      ["image"]
+    ]
+  },
+  placeholder: "asd",
+  theme: "snow"
+});
 			</script>
 			';
 		}
