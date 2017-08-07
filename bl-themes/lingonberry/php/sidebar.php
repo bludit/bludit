76aa41823237b8622b66d8904dd2ce8d441881dc
Diego Najar <dignajar@gmail.com>
2017-08-07 21:10:38 +0200

    RSS and Sitemap fixed links

--- bl-themes/lingonberry/php/sidebar.php
+++ bl-themes/lingonberry/php/sidebar.php
@@ -8,7 +8,7 @@
 							<?php
 								$pages = $dbPages->getList(1, 5, true);
 								$keys = array_keys($pages);
-								
+
 								foreach($keys as $pageKey) {
 									$page = buildPage($pageKey);
 									echo '<li><a href="'.$page->permalink().'">'.$page->title().'</a></li>';
@@ -20,7 +20,7 @@
 			</div>
 			<div class="clear"></div>
 		</div>
-	
+
 		<div class="footer-b widgets">
 			<div class="widget widget_recent_entries">
 				<div class="widget-content">
@@ -29,19 +29,19 @@
 							<?php
 								$db = $dbTags->db;
 								$filter = $Url->filters('tag');
-								
+
 								$tagArray = array();
-								
+
 								foreach($db as $tagKey=>$fields) {
 									$tagArray[] = array('tagKey'=>$tagKey, 'name'=>$fields['name']);
 								}
-								
+
 								$tagArray = array_slice($tagArray, 0, 5);
-								
+
 								usort($tagArray, function($a, $b) {
 									return strcmp($a['tagKey'], $b['tagKey']);
 								});
-								
+
 								foreach($tagArray as $tagKey=>$fields) {
 									echo '<li><a href="'.HTML_PATH_ROOT.$filter.'/'.$fields['tagKey'].'">'.$fields['name'].'</a></li>';
 								}
@@ -52,18 +52,18 @@
 			</div>
 			<div class="clear"></div>
 		</div>
-	
+
 		<div class="footer-c widgets">
 			<div class="widget-content">
 					<h3 class="widget-title">Links</h3>
 						<ul>
-							<li><a href="<?php echo $Site->url() ?>sitemap.xml">Sitemap</a></li>
-							<li><a href="<?php echo $Site->url() ?>rss.xml">RSS Feed</a></li>
+							<li><a href="<?php echo Theme::sitemapUrl() ?>">Sitemap</a></li>
+							<li><a href="<?php echo Theme::rssUrl()?>">RSS Feed</a></li>
 						</ul>
 				</div>
 			<div class="clear"></div>
 		</div>
-			
+
 		<div class="clear"></div>
 	</div> <!-- /footer-inner -->
 </div> <!-- /footer -->
