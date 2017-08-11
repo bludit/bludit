647d623f1c821df75ae74c22cab3168bc5fbc197
Diego Najar <dignajar@gmail.com>
2017-08-11 21:26:28 +0200

    External Cover Image

--- bl-kernel/admin/views/new-page.php
+++ bl-kernel/admin/views/new-page.php
@@ -120,9 +120,8 @@ echo '<div class="bl-publish-sidebar uk-width-2-10">';
 		'class'=>'uk-width-1-1 uk-form-medium',
 		'options'=>array(
 			'published'=>$L->g('Published'),
-			'draft'=>$L->g('Draft'),
 			'fixed'=>$L->g('Fixed'),
-			'sticky'=>$L->g('Sticky')
+			'draft'=>$L->g('Draft')
 		),
 		'selected'=>'published',
 		'tip'=>''
