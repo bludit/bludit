6b15fcb4275a96b2ef6b02881b02968332fb310a
Diego Najar <dignajar@gmail.com>
2017-09-04 00:12:36 +0200

    Spanish and German lang

---
+++ lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$string = file_get_contents("bl-languages/en.json");
+$english = json_decode($string, true);
+
+$string = file_get_contents("bl-languages/de_DE.json");
+$spanish = json_decode($string, true);
+
+$tmp = array();
+foreach($english as $key=>$value) {
+	if (isset($spanish[$key])) {
+		$tmp[$key] = $spanish[$key];
+	} else {
+		$tmp[$key] = $value;
+	}
+}
+
+echo json_encode($tmp, JSON_UNESCAPED_UNICODE);
