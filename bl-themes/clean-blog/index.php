cbc2b1ddd487f9b6e554bdbcc8a08383e863e5f1
Diego Najar <dignajar@gmail.com>
2017-10-14 14:30:47 +0200

    Missing phrase on opengraph and twitter cards

--- bl-themes/clean-blog/index.php
+++ bl-themes/clean-blog/index.php
@@ -16,14 +16,14 @@
       <div class="container">
         <a class="navbar-brand" href="<?php echo $Site->url() ?>"><?php echo $Site->title() ?></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
+	<?php echo $Language->get('Menu') ?>
           <i class="fa fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href=<?php echo $Site->url() ?>Home</a>
-	          </li>
+              <a class="nav-link" href="<?php echo $Site->url() ?>"><?php echo $Language->get('Home') ?></a>
+	    </li>
             <?php
               foreach ($staticPages as $staticPage) {
                 echo '<li class="nav-item">';
