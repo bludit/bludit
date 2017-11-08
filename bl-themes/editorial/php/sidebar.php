17e35fd0533ea593d4c1e6486e0593b9e5173b08
Diego Najar <dignajar@gmail.com>
2017-11-08 23:18:41 +0100

    remove themes

--- bl-themes/editorial/php/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php Theme::plugins('siteSidebar') ?>
-
-<!-- Footer -->
-<footer id="footer">
-        <p class="copyright"><?php echo $Site->footer() ?><br>Design: <a href="https://html5up.net">HTML5 UP</a><br>Powered by <a href="https://www.bludit.com">BLUDIT</a></p>
-</footer>
