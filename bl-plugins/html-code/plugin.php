d4bda555213b3a20ddc4a480cd29086603e534c0
Diego Najar <dignajar@gmail.com>
2018-01-07 12:45:47 +0100

    New plugin HTML Code

--- bl-plugins/html-code/plugin.php
+++ bl-plugins/html-code/plugin.php
@@ -5,6 +5,7 @@ class pluginHTMLCode extends Plugin {
 	public function init()
 	{
 		$this->dbFields = array(
+			'head'=>'',
 			'header'=>'',
 			'footer'=>''
 		);
@@ -12,20 +13,35 @@ class pluginHTMLCode extends Plugin {
 
 	public function form()
 	{
+		global $Language;
+
 		$html  = '<div>';
-		$html .= '<label>Header</label>';
-		$html .= '<textarea name="header" id="jsheader">'.$this->getValue('header').'</textarea>';
+		$html .= '<label>Theme Head</label>';
+		$html .= '<textarea name="head" id="jshead">'.$this->getValue('head').'</textarea>';
+		$html .= '<span class="tip">'.$Language->get('insert-code-in-the-theme-inside-the-tag-head').'</span>';
 		$html .= '</div>';
 
 		$html .= '<div>';
-		$html .= '<label>Footer</label>';
+		$html .= '<label>Theme Header</label>';
+		$html .= '<textarea name="header" id="jsheader">'.$this->getValue('header').'</textarea>';
+		$html .= '<span class="tip">'.$Language->get('insert-code-in-the-theme-at-the-top').'</span>';
+		$html .= '</div>';
+
+		$html .= '<div>';
+		$html .= '<label>Theme Footer</label>';
 		$html .= '<textarea name="footer" id="jsfooter">'.$this->getValue('footer').'</textarea>';
+		$html .= '<span class="tip">'.$Language->get('insert-code-in-the-theme-at-the-bottom').'</span>';
 		$html .= '</div>';
 
 		return $html;
 	}
 
 	public function siteHead()
+	{
+		return html_entity_decode($this->getValue('head'));
+	}
+
+	public function siteBodyBegin()
 	{
 		return html_entity_decode($this->getValue('header'));
 	}
