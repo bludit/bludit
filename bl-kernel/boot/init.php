62b30f4381570f1106b25391a68f5c68ed269331
Diego Najar <dignajar@gmail.com>
2018-01-22 21:58:07 +0100

    Bludit v2.2.0

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -1,10 +1,10 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
 // Bludit version
-define('BLUDIT_VERSION',	'2.1.2');
+define('BLUDIT_VERSION',	'2.2.0');
 define('BLUDIT_CODENAME',	'Rick');
-define('BLUDIT_RELEASE_DATE',	'2018-01-21');
-define('BLUDIT_BUILD',		'20180121');
+define('BLUDIT_RELEASE_DATE',	'2018-01-22');
+define('BLUDIT_BUILD',		'20180122');
 
 // Debug mode
 // Change to FALSE, for prevent warning or errors on browser
