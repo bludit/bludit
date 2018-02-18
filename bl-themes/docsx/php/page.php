7183a4cab2583a4c7ccf904fcc1af8cf865beb3c
Diego Najar <dignajar@gmail.com>
2018-02-18 20:46:48 +0100

    Docs X theme

--- bl-themes/docsx/php/page.php
+++ bl-themes/docsx/php/page.php
@@ -5,3 +5,7 @@
 <div id="page-content">
 <?php echo $page->content(); ?>
 </div>
+
+<div class="text-right mt-5">
+	<a class="btn btn-primary" href="<?php echo $GITHUB_BASE_URL.$page->key().'/'.FILENAME ?>">Collaborate with us and edit this page</a>
+</div>
