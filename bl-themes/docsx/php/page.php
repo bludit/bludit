d6e6530be910b9b9243e3b2b7950dcf10a1f30ac
floppy0 <31189947+floppy0@users.noreply.github.com>
2018-02-23 15:53:16 +0100

    hide editing button

--- bl-themes/docsx/php/page.php
+++ bl-themes/docsx/php/page.php
@@ -6,6 +6,8 @@
 <?php echo $page->content(); ?>
 </div>
 
+<?php if (!$Url->notFound()): ?>
 <div class="text-right mt-5">
 	<a class="btn btn-primary" href="<?php echo $GITHUB_BASE_URL.$page->key().'/'.FILENAME ?>"><?php echo $Language->get('Collaborate with us and edit this page'); ?></a>
 </div>
+<?php endif ?>
