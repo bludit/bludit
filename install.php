0995fc8eea423fa5123401c63cd0779186330133
floppy0 <31189947+floppy0@users.noreply.github.com>
2018-03-02 15:10:48 +0100

    try to create the .htaccess

--- install.php
+++ install.php
@@ -190,19 +190,36 @@ function checkSystem()
 	$stdOut = array();
 	$dirpermissions = 0755;
 
-	// Check .htaccess file for different webservers
-	if (!file_exists(PATH_ROOT.'.htaccess')) {
+	// Try to create .htaccess file if not exists
+	$htaccessContent = 'AddDefaultCharset UTF-8
 
-		if (	!isset($_SERVER['SERVER_SOFTWARE']) ||
-			stripos($_SERVER['SERVER_SOFTWARE'], 'Apache') !== false ||
-			stripos($_SERVER['SERVER_SOFTWARE'], 'LiteSpeed') !== false
-		) {
-			$errorText = 'Missing file, upload the file .htaccess  (ERROR_204)';
-			error_log($errorText, 0);
+<IfModule mod_rewrite.c>
 
-			$tmp['title'] = 'File .htaccess';
-			$tmp['errorText'] = $errorText;
-			array_push($stdOut, $tmp);
+# Enable rewrite rules
+RewriteEngine on
+
+# Deny direct access to .txt files
+RewriteRule ^bl-content/(.*)\.txt$ - [R=404,L]
+
+# All URL process by index.php
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule ^(.*) index.php [PT,L]
+
+</IfModule>';
+
+	if ( !file_exists(PATH_ROOT.'.htaccess') || (filesize(PATH_ROOT.'.htaccess')<1) ) {
+		if (!file_put_contents(PATH_ROOT.'.htaccess', $htaccessContent)) {
+			if (!empty($_SERVER['SERVER_SOFTWARE'])) {
+				$webserver = Text::lowercase($_SERVER['SERVER_SOFTWARE']);
+				if (Text::stringContains($webserver, 'apache') || Text::stringContains($webserver, 'litespeed')) {
+					$errorText = 'Missing file, upload the file .htaccess (ERROR_204)';
+					error_log($errorText, 0);
+
+					$tmp['title'] = 'File .htaccess';
+					$tmp['errorText'] = $errorText;
+					array_push($stdOut, $tmp);
+				}
+			}
 		}
 	}
 
