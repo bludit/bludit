b75288e36235426acf46e4e9e5af41332a559114
Diego Najar <dignajar@gmail.com>
2018-03-28 23:34:36 +0200

    GDPR

--- bl-kernel/js/functions.php
+++ bl-kernel/js/functions.php
@@ -68,4 +68,29 @@ function sanitizeHTML(text) {
 	return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
 
+function setCookie(name, value, days) {
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days*24*60*60*1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return false;
+}
+
+function deleteCookie(name) {
+	document.cookie = name+'=; Max-Age=-999;';
+}
+
 </script>
