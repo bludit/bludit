0a1a97893faf68257556dbb5311a2431cda4c3fd
Diego Najar <dignajar@gmail.com>
2019-01-25 16:26:34 +0100

    Plugin HTML Code added support for admin area

--- bl-plugins/html-code/plugin.php
+++ bl-plugins/html-code/plugin.php
@@ -7,7 +7,10 @@ class pluginHTMLCode extends Plugin {
 		$this->dbFields = array(
 			'head'=>'',
 			'header'=>'',
-			'footer'=>''
+			'footer'=>'',
+			'adminHead'=>'',
+			'adminHeader'=>'',
+			'adminFooter'=>''
 		);
 	}
 
@@ -19,21 +22,43 @@ class pluginHTMLCode extends Plugin {
 		$html .= $this->description();
 		$html .= '</div>';
 
+		$html .= '<h2>'.$L->g('Website').'</h2>';
+
 		$html .= '<div>';
-		$html .= '<label>Site Head</label>';
+		$html .= '<label>Head</label>';
 		$html .= '<textarea name="head" id="jshead">'.$this->getValue('head').'</textarea>';
 		$html .= '<span class="tip">'.$L->get('insert-code-in-the-theme-inside-the-tag-head').'</span>';
 		$html .= '</div>';
 
 		$html .= '<div>';
-		$html .= '<label>Site Header</label>';
+		$html .= '<label>Header</label>';
 		$html .= '<textarea name="header" id="jsheader">'.$this->getValue('header').'</textarea>';
 		$html .= '<span class="tip">'.$L->get('insert-code-in-the-theme-at-the-top').'</span>';
 		$html .= '</div>';
 
 		$html .= '<div>';
-		$html .= '<label>Site Footer</label>';
+		$html .= '<label>Footer</label>';
 		$html .= '<textarea name="footer" id="jsfooter">'.$this->getValue('footer').'</textarea>';
+		$html .= '<span class="tip">'.$L->get('insert-code-in-the-theme-at-the-bottom').'</span>';
+		$html .= '</div>';
+
+		$html .= '<h2 class="mt-4">'.$L->g('Admin area').'</h2>';
+
+		$html .= '<div>';
+		$html .= '<label>Head</label>';
+		$html .= '<textarea name="adminHead">'.$this->getValue('adminHead').'</textarea>';
+		$html .= '<span class="tip">'.$L->get('insert-code-in-the-theme-inside-the-tag-head').'</span>';
+		$html .= '</div>';
+
+		$html .= '<div>';
+		$html .= '<label>Header</label>';
+		$html .= '<textarea name="adminHeader">'.$this->getValue('adminHeader').'</textarea>';
+		$html .= '<span class="tip">'.$L->get('insert-code-in-the-theme-at-the-top').'</span>';
+		$html .= '</div>';
+
+		$html .= '<div>';
+		$html .= '<label>Footer</label>';
+		$html .= '<textarea name="adminFooter">'.$this->getValue('adminFooter').'</textarea>';
 		$html .= '<span class="tip">'.$L->get('insert-code-in-the-theme-at-the-bottom').'</span>';
 		$html .= '</div>';
 
@@ -53,5 +78,20 @@ class pluginHTMLCode extends Plugin {
 	public function siteBodyEnd()
 	{
 		return html_entity_decode($this->getValue('footer'));
+	}
+
+	public function adminHead()
+	{
+		return html_entity_decode($this->getValue('adminHead'));
+	}
+
+	public function adminBodyBegin()
+	{
+		return html_entity_decode($this->getValue('adminHeader'));
+	}
+
+	public function adminBodyEnd()
+	{
+		return html_entity_decode($this->getValue('adminFooter'));
 	}
 }
