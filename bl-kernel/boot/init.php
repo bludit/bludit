6db9c59dd57d2358d3d50d8cc6263f85c21ced70
Diego Najar <dignajar@gmail.com>
2019-01-28 22:20:15 +0100

    change metadata for Bludit v3.7.1

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -1,10 +1,10 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
 // Bludit version
-define('BLUDIT_VERSION',	'3.7.0');
+define('BLUDIT_VERSION',	'3.7.1');
 define('BLUDIT_CODENAME',	'Bock');
-define('BLUDIT_RELEASE_DATE',	'2019-01-27');
-define('BLUDIT_BUILD',		'20190127');
+define('BLUDIT_RELEASE_DATE',	'2019-01-28');
+define('BLUDIT_BUILD',		'20190128');
 
 // Debug mode
 // Change to FALSE, for prevent warning or errors on browser
