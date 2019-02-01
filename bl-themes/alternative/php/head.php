de9919034177156070676d35fa4da4d79bd27d18
Diego Najar <dignajar@gmail.com>
2019-02-01 17:59:15 +0100

    Change tag and include generator tag

--- bl-themes/alternative/php/head.php
+++ bl-themes/alternative/php/head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="author" content="Bludit CMS">
+<meta name="generator" content="Bludit">
 
 <!-- Dynamic title tag -->
 <?php echo Theme::metaTags('title'); ?>
