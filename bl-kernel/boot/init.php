c72f93601742be5ae33d4575951f6206f6ed0bc0
Diego Najar <dignajar@gmail.com>
2019-02-28 15:59:59 +0100

    changed metadata for 3.8.1

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -1,10 +1,10 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
 // Bludit version
-define('BLUDIT_VERSION',	'3.8.0');
+define('BLUDIT_VERSION',	'3.8.1');
 define('BLUDIT_CODENAME',	'APA');
-define('BLUDIT_RELEASE_DATE',	'2019-02-22');
-define('BLUDIT_BUILD',		'20190222');
+define('BLUDIT_RELEASE_DATE',	'2019-02-28');
+define('BLUDIT_BUILD',		'20190228');
 
 // Debug mode
 // Change to FALSE, for prevent warning or errors on browser
