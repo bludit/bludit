3d87b6597d748e30cd4f4487db2a70e01d419852
Diego Najar <dignajar@gmail.com>
2019-05-12 14:50:54 +0200

    Editor role now only can edit his own content

--- bl-kernel/admin/controllers/content.php
+++ bl-kernel/admin/controllers/content.php
@@ -42,7 +42,7 @@ $static = $pages->getStaticDB(true);
 $sticky = $pages->getStickyDB(true);
 
 // If the user is an Editor filter the content he/she can edit
-if (checkRole(array('editor'))) {
+if (checkRole(array('editor'), false)) {
 	$published 	= filterContentOwner($published);
 	$drafts 	= filterContentOwner($drafts);
 	$scheduled 	= filterContentOwner($scheduled);
