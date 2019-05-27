49027a3bad957e00c083c0265e1fcb86798634c6
Diego Najar <dignajar@gmail.com>
2019-05-27 21:56:52 +0200

    Bludit v3.9.0

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -1,10 +1,10 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
 // Bludit version
-define('BLUDIT_VERSION',	'3.9.0-beta');
-define('BLUDIT_CODENAME',	'Github version');
-define('BLUDIT_RELEASE_DATE',	'2019-05-18');
-define('BLUDIT_BUILD',		'20190518');
+define('BLUDIT_VERSION',	'3.9.0');
+define('BLUDIT_CODENAME',	'Porter');
+define('BLUDIT_RELEASE_DATE',	'2019-05-27');
+define('BLUDIT_BUILD',		'20190527');
 
 // Debug mode
 // Change to FALSE, for prevent warning or errors on browser
