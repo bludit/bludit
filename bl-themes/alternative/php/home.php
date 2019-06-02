1ea94a4f0b51824ea7d477aa2f80d3503d8e5085
Diego Najar <dignajar@gmail.com>
2019-06-02 20:37:45 +0200

    Include RSS link

--- bl-themes/alternative/php/home.php
+++ bl-themes/alternative/php/home.php
@@ -12,12 +12,20 @@
 		<!-- Custom search form if the plugin "search" is enabled -->
 		<?php if (pluginActivated('pluginSearch')): ?>
 		<div class="form-inline d-block">
-			<input id="search-input" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			<button class="btn btn-outline-primary my-2 my-sm-0" type="button" onClick="searchNow()">Search</button>
+			<input id="search-input" class="form-control mr-sm-2" type="search" placeholder="<?php $language->p('Search') ?>" aria-label="Search">
+			<button class="btn btn-outline-primary my-2 my-sm-0" type="button" onClick="searchNow()"><?php $language->p('Search') ?></button>
 			<script>
 				function searchNow() {
 					var searchURL = "<?php echo Theme::siteUrl(); ?>search/";
 					window.open(searchURL + document.getElementById("search-input").value, "_self");
+				}
+				document.getElementById("search-input").onkeypress = function(e) {
+					if (!e) e = window.event;
+					var keyCode = e.keyCode || e.which;
+					if (keyCode == '13') {
+						searchNow();
+						return false;
+					}
 				}
 			</script>
 		</div>
