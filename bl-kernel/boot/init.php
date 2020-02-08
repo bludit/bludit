29c8341b2f083f5c25f55ace23b12dd185ef2074
Diego Najar <dignajar@gmail.com>
2020-02-08 19:24:17 +0100

    Bludit v3.11.0

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -1,10 +1,10 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
 // Bludit version
-define('BLUDIT_VERSION',	'3.10.0');
-define('BLUDIT_CODENAME',	'Mateo');
-define('BLUDIT_RELEASE_DATE',	'2019-10-19');
-define('BLUDIT_BUILD',		'20191019');
+define('BLUDIT_VERSION',	'3.11.0');
+define('BLUDIT_CODENAME',	'Pizza time');
+define('BLUDIT_RELEASE_DATE',	'2020-02-08');
+define('BLUDIT_BUILD',		'20200208');
 
 // Debug mode
 // Change to FALSE, for prevent warning or errors on browser
