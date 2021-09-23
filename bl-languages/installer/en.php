272cc5bad01f296456d124fd87af65159b29900b
Diego Najar <dignajar@gmail.com>
2021-09-23 18:48:11 +0200

    examples pages per language

---
+++ bl-languages/installer/en.php
@@ -0,0 +1,51 @@
+<?php
+$examples = array(
+    array(
+        'url' => 'welcome-to-bludit',
+        'type' => 'published',
+        'category' => 'general',
+        'position' => 1,
+        'title' => 'Welcome to Bludit!',
+        'description' => 'A warm welcome to your new website.',
+        'tags' => array('Welcome', 'Plugins', 'Themes', 'Bludit'),
+        'content' =>
+'<p>Thanks for chose Bludit and welcome to our community, we are growing up everyday and is awesome.</p>
+<p>Follow Bludit in different social networks:</p>
+<ul>
+<li><a href="https://twitter.com/bludit">https://twitter.com/bludit</a></li>
+<li><a href="https://www.facebook.com/bluditcms">https://www.facebook.com/bluditcms</a></li>
+</ul>
+<p>Take a look to the plugins and themes:</p>
+<ul>
+<li><a href="https://plugins.bludit.com">https://plugins.bludit.com</a></li>
+<li><a href="https://themes.bludit.com">https://themes.bludit.com</a></li>
+</ul>
+<p>Do you need support or you want to read about Bludit, check our official documentation or participate in our forum.</p>
+<ul>
+<li><a href="https://forum.bludit.org">https://forum.bludit.org</a></li>
+<li><a href="https://docs.bludit.com">https://docs.bludit.com</a></li>
+</ul>'
+    ),
+    array(
+        'url' => 'set-up-your-new-website',
+        'type' => 'published',
+        'category' => 'general',
+        'position' => 2,
+        'title' => 'Set up your new website',
+        'description' => 'Configure your new website.',
+        'tags' => array('Language', 'Timezone', 'Markdown', 'Bludit'),
+        'content' =>
+'<h2>Site information</h2>
+<p>Improve your site information and metadata. Include a site description to refine your ranking on the search engines, add social networks, include a site logo with your brand.</p>
+<h2>Language and Timezone</h2>
+<p>Bludit supports a variety of languages and time zones. Choose the option that best suits you and your visitors.</p>
+<h2>Markdown</h2>
+<p>Bludit supports Markdown, you just need to enable the right editor to work with it.</p>
+<ol>
+<li>Go to the Admin panel</li>
+<li>Go to Plugins section</li>
+<li>Disable TinyMCE plugin</li>
+<li>Enable EasyMDE plugin</li>
+</ol>'
+    )
+);
