76e85f4082fd2dd0747cee56fedf4c3dabe0869c
Tim Moritz <tim_moritz@cds-service.com>
2022-03-17 14:16:01 +0100

    add missing api endpoint (GET) /api/pages

--- bl-plugins/api/plugin.php
+++ bl-plugins/api/plugin.php
@@ -149,8 +149,12 @@ class pluginAPI extends Plugin {
         // /api/pages/:key
         // /api/pages/:parent/:key
 
+        // (GET) /api/pages
+        if ( ($method==='GET') && ($parmA==='pages') && empty($parmB) ) {
+            $data = $this->getPages($inputs);
+        }
         // (GET) /api/pages/files/:key
-        if ( ($method==='GET') && ($parmA==='pages') && ($parmB==='files') && !empty($parmC) ) {
+        elseif ( ($method==='GET') && ($parmA==='pages') && ($parmB==='files') && !empty($parmC) ) {
             $key = $parmC;
             if (!empty($parmD)) {
                 $key = $parmC.'/'.$parmD;
