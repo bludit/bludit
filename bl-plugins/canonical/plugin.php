f50df101eb03ed40e23d9c62eed6627600fda119
Elixcz <109692427+Elixcz@users.noreply.github.com>
2022-09-30 21:24:53 +0200

    [Plugin] Canonical - added support fot category and tags URLs. Added Czech language

--- bl-plugins/canonical/plugin.php
+++ bl-plugins/canonical/plugin.php
@@ -3,11 +3,43 @@
 class pluginCanonical extends Plugin {
 
 	public function siteHead() {
-		if ($GLOBALS['WHERE_AM_I'] === 'home') {
-			return '<link rel="canonical" href="'.DOMAIN_BASE.'"/>'.PHP_EOL;
-		} elseif ($GLOBALS['WHERE_AM_I'] === 'page') {
+
+		$html = '<!-- Plugin Canonical -->' . PHP_EOL;
+
+		if ($GLOBALS['WHERE_AM_I'] === 'home')
+		{
+			$html .= '<link rel="canonical" href="'.DOMAIN_BASE.'">'.PHP_EOL;
+
+		} elseif ($GLOBALS['WHERE_AM_I'] === 'page')
+		{
 			global $page;
-			return '<link rel="canonical" href="'.$page->permalink().'"/>'.PHP_EOL;
+			$html .= '<link rel="canonical" href="'.$page->permalink().'">'.PHP_EOL;
+
+		} elseif ($GLOBALS['WHERE_AM_I'] === 'category')
+		{
+			global $url;
+			$categoryKey = $url->slug();
+			$category = new Category( $categoryKey );
+
+			$html .= '<link rel="canonical" href="' . $category->permalink() . '">'.PHP_EOL;
+
+		} elseif ($GLOBALS['WHERE_AM_I'] === 'tag')
+		{
+			global $url;
+			$tagKey = $url->slug();
+			$tag = new Tag( $tagKey );
+
+			$html .= '<link rel="canonical" href="'.$tag->permalink().'">'.PHP_EOL;
+
 		}
+		$html .= '<!-- /Plugin Canonical -->' . PHP_EOL;
+		return $html;
+	}
+
+	public function form()
+	{
+		global $L;
+
+		return '<div class="alert alert-info">' . $L->g('This plugin has no settings') . '</div>';
 	}
 }
