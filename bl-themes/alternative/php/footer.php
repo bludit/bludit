0b381e8df750f3322dc119d7212be67b097e4aea
Duncan Cameron <3147688+bramley@users.noreply.github.com>
2024-03-24 12:32:29 +0000

    Remove spurious </span>

--- bl-themes/alternative/php/footer.php
+++ bl-themes/alternative/php/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer bg-dark">
-	<div class="container">
-		<p class="m-0 text-center text-white text-uppercase"><?php echo $site->footer(); ?><span class="ml-5 text-warning"><img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/> Powered by <a target="_blank" class="text-white" href="https://www.bludit.com"><?php echo (defined('BLUDIT_PRO'))?'BLUDIT PRO':'BLUDIT' ?></span></a></span></p>
-	</div>
+    <div class="container">
+        <p class="m-0 text-center text-white text-uppercase"><?php echo $site->footer(); ?><span class="ml-5 text-warning"><img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/> Powered by <a target="_blank" class="text-white" href="https://www.bludit.com"><?php echo (defined('BLUDIT_PRO'))?'BLUDIT PRO':'BLUDIT' ?></a></span></p>
+    </div>
 </footer>
