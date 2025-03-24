ddd7d21ee4e7aa69a05287d6438b94ae145da517
Christian Lauf <github.com@christian-lauf.info>
2025-03-25 00:05:09 +0100

    Fix canoncial links in siteHead plugin
    W3C Validator gives the following Info regarding the currently generated trailing slash on link elements: Trailing slash on void elements has no effect and interacts badly with unquoted attribute values.

--- bl-plugins/canonical/plugin.php
+++ bl-plugins/canonical/plugin.php
@@ -5,10 +5,10 @@ class pluginCanonical extends Plugin {
 	public function siteHead()
 	{
 		if ($GLOBALS['WHERE_AM_I'] === 'home') {
-			return '<link rel="canonical" href="'.DOMAIN_BASE.'"/>'.PHP_EOL;
+			return '<link rel="canonical" href="'.DOMAIN_BASE.'">'.PHP_EOL;
 		} elseif ($GLOBALS['WHERE_AM_I'] === 'page') {
 			global $page;
-			return '<link rel="canonical" href="'.$page->permalink().'"/>'.PHP_EOL;
+			return '<link rel="canonical" href="'.$page->permalink().'">'.PHP_EOL;
 		}
 	}
 
