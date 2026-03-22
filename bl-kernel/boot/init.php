072d64b4fde19b53a43c831a7a34a7eedd61074d
dignajar <dignajar@gmail.com>
2026-03-22 15:51:48 +0100

    chore: bump version

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -1,10 +1,10 @@
 <?php defined('BLUDIT') or die('Bludit CMS.');
 
 // Bludit version
-define('BLUDIT_VERSION',        '3.18.4');
+define('BLUDIT_VERSION',        '3.19.0');
 define('BLUDIT_CODENAME',       'Lince');
-define('BLUDIT_RELEASE_DATE',   '2026-03-17');
-define('BLUDIT_BUILD',          '20260317');
+define('BLUDIT_RELEASE_DATE',   '2026-03-22');
+define('BLUDIT_BUILD',          '20260322');
 
 // Change to TRUE for debugging
 define('DEBUG_MODE', TRUE);
